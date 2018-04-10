07909be854623c7143ee2eac979c32a373b4f570
David Bristoll <36759641+Pringleman83@users.noreply.github.com>
2018-04-10 11:33:16 +0100

    Update Second_Timer.ino.ino
    Changed the variable type of the startLoopSeconds and endLoopSeconds variables to unsigned long int type so that the code can run for longer than the 8 hours a standard int type variable would allow.
    
    Added a check for endLoopSeconds equalling zero which, when not present, resulted in code executing multiple times during the first second of activation of the Arduino.

--- Second_Timer.ino.ino
+++ Second_Timer.ino.ino
@@ -8,8 +8,8 @@
  */
 
 //Important variables - Place this section at the top of your code.
-int startLoopSeconds = 0;
-int endLoopSeconds = 0;
+unsigned long startLoopSeconds = 0;
+unsigned long endLoopSeconds = 0;
 unsigned long previousMillis = millis();
 unsigned long previousMillisOld;
 //End of important variables.
@@ -33,7 +33,7 @@ previousMillis = millis();
 //Add your code here
 
 //End of loop timer - Place this at the end of your main loop().
-if(millis() - previousMillisOld >= 1000){
+if(millis() - previousMillisOld >= 1000 || endLoopSeconds == 0){
   endLoopSeconds++;
 }
 //End of end of loop timer.
